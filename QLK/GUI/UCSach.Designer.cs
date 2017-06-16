99274f99d3d78703c4011d8edd5ee9ff314b80bd
ngabao <nguyenthiennga.mingo@gmail.com>
2017-06-16 11:13:02 +0700

    UCSach

---
+++ QLK/GUI/UCSach.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GUI
+{
+    partial class UCSach
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tựaSáchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thểLoạiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nhàXuấtBảnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.RB1 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tựaSáchToolStripMenuItem,
+            this.thểLoạiToolStripMenuItem,
+            this.nhàXuấtBảnToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(790, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tựaSáchToolStripMenuItem
+            // 
+            this.tựaSáchToolStripMenuItem.Name = "tựaSáchToolStripMenuItem";
+            this.tựaSáchToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.tựaSáchToolStripMenuItem.Text = "Tựa sách";
+            this.tựaSáchToolStripMenuItem.Click += new System.EventHandler(this.tựaSáchToolStripMenuItem_Click);
+            // 
+            // thểLoạiToolStripMenuItem
+            // 
+            this.thểLoạiToolStripMenuItem.Name = "thểLoạiToolStripMenuItem";
+            this.thểLoạiToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.thểLoạiToolStripMenuItem.Text = "Thể loại";
+            this.thểLoạiToolStripMenuItem.Click += new System.EventHandler(this.thểLoạiToolStripMenuItem_Click);
+            // 
+            // nhàXuấtBảnToolStripMenuItem
+            // 
+            this.nhàXuấtBảnToolStripMenuItem.Name = "nhàXuấtBảnToolStripMenuItem";
+            this.nhàXuấtBảnToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.nhàXuấtBảnToolStripMenuItem.Text = "Nhà xuất bản";
+            this.nhàXuấtBảnToolStripMenuItem.Click += new System.EventHandler(this.nhàXuấtBảnToolStripMenuItem_Click);
+            // 
+            // RB1
+            // 
+            this.RB1.Location = new System.Drawing.Point(0, 30);
+            this.RB1.Name = "RB1";
+            this.RB1.Size = new System.Drawing.Size(790, 385);
+            this.RB1.TabIndex = 3;
+            // 
+            // UCSach
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.RB1);
+            this.Name = "UCSach";
+            this.Size = new System.Drawing.Size(790, 416);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tựaSáchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thểLoạiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nhàXuấtBảnToolStripMenuItem;
+        private System.Windows.Forms.Panel RB1;
+    }
+}
